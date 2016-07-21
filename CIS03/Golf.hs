950951b898ca554b19c60d9b262afb14f202edae
yangwjd <yangwjd@chanjet.com>
2016-07-21 14:40:15 +0800

    CIS03

---
+++ CIS03/Golf.hs
@@ -0,0 +1,40 @@
+module Golf where
+
+-- Exercise 1 Hopscotch
+skip :: Int -> [a] -> [a]
+skip n xs = map fst $ filter (\x -> snd x `mod` n == 0) $ zip xs [1..]
+skips :: [a] -> [[a]]
+skips xs = map (\x -> skip x xs) index
+  where
+    index = [1..(length xs)]
+
+-- Exercise 2 Local maxima
+localMaxima :: [Integer] -> [Integer]
+localMaxima (x:xs@(y:z:ys))
+    | y > x && y > z = y : rest
+    | otherwise      = rest
+    where
+      rest = localMaxima xs
+
+localMaxima _        = []
+
+-- Exercise 3 Histogram
+-- 统计数字出现次数
+calDigit :: [Int] -> [Int]
+calDigit xs = map f [0..9]
+  where
+    f :: Int -> Int
+    f x = length (filter (==x) xs)
+
+-- 将出现次数映射成直方图形式的字符串
+makeStr :: [Int] -> String
+makeStr xs | all (<= 0) xs = ""
+           | otherwise     = (makeStr rest) ++ newLine ++ "\n"
+           where
+             newLine = map (\x -> if x > 0 then '*' else '.') xs
+             rest    = map (\x -> x - 1) xs
+
+histogram :: [Int] -> String
+histogram xs = (makeStr $ calDigit xs) ++ "==========\n0123456789\n"
+
+
